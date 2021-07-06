f1f16f68db132285fd79ceec3fbde5ba292e68d3
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-06 20:00:15 +0530

    initialize a String object with the method call of the method getText() followed by the method toString()

--- app/src/main/java/com/example/signin_signup_app_java/MainActivity.java
+++ app/src/main/java/com/example/signin_signup_app_java/MainActivity.java
@@ -36,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                String email = emailField.getText().toString();
             }
         });
     }
