0f3a3095c98f126da4f06d45cb80668021a2d8ed
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-06 21:27:46 +0530

    initialize the object nameField

--- app/src/main/java/com/example/signin_signup_app_java/SignUpPage.java
+++ app/src/main/java/com/example/signin_signup_app_java/SignUpPage.java
@@ -14,5 +14,6 @@ public class SignUpPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up_page);
+        nameField = findViewById(R.id.editTextTextPersonName3);
     }
 }
