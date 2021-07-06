a2640bf4ed3fb5b2fe4cbbc6fe666194c9dfd23c
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-06 19:55:37 +0530

    initialize the object passwordField

--- app/src/main/java/com/example/signin_signup_app_java/MainActivity.java
+++ app/src/main/java/com/example/signin_signup_app_java/MainActivity.java
@@ -17,5 +17,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         emailField = findViewById(R.id.editTextTextPersonName);
+        passwordField = findViewById(R.id.editTextTextPersonName2);
     }
 }
