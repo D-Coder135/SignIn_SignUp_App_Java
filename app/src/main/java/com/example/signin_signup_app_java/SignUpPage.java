3954dd76d22f31ae3e6b4e9e2396f1fcb1dd1773
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-06 21:27:01 +0530

    define the objects for the class Button

--- app/src/main/java/com/example/signin_signup_app_java/SignUpPage.java
+++ app/src/main/java/com/example/signin_signup_app_java/SignUpPage.java
@@ -1,12 +1,14 @@
 package com.example.signin_signup_app_java;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SignUpPage extends AppCompatActivity {
     EditText nameField, emailField, passwordField;
+    Button signupButton, backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
