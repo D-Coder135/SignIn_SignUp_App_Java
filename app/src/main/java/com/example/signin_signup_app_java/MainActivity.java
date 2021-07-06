93835460abd40aecabd8b99645cab10093f7f144
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-06 19:58:32 +0530

    call the startActivity() method along with the intent object as its parameter

--- app/src/main/java/com/example/signin_signup_app_java/MainActivity.java
+++ app/src/main/java/com/example/signin_signup_app_java/MainActivity.java
@@ -28,6 +28,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, SignUpPage.class);
+                startActivity(intent);
+
             }
         });
     }
