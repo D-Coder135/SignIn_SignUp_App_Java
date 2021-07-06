55c5d1937ecc37115239fd4018e4eb1116969580
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-06 21:31:18 +0530

    call the finish() method

--- app/src/main/java/com/example/signin_signup_app_java/SignUpPage.java
+++ app/src/main/java/com/example/signin_signup_app_java/SignUpPage.java
@@ -27,6 +27,7 @@ public class SignUpPage extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(SignUpPage.this, MainActivity.class);
                 startActivity(intent);
+                finish();
             }
         });
     }
