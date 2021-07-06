4ae8df4bbfa98fa1b96ccb26592c6c8cfd4fbbfd
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-06 22:15:13 +0530

    define the object for the class Intent

--- app/src/main/java/com/example/signin_signup_app_java/WelcomePage.java
+++ app/src/main/java/com/example/signin_signup_app_java/WelcomePage.java
@@ -1,5 +1,6 @@
 package com.example.signin_signup_app_java;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -19,7 +20,7 @@ public class WelcomePage extends AppCompatActivity {
         logoutButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent intent = new Intent(WelcomePage.this, MainActivity.class);
             }
         });
     }
